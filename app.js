7c5a1fbf59ae2ad9efdffdae3d1b4d9f914ce75b
aditi walia <waliaaditi93@gmail.com>
2022-12-29 23:16:57 +0530

    changes added

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var btnTranslate = document.querySelector("#btn-translate");
 var txtInput = document.querySelector("#txt-input");
 var outputDiv = document.querySelector("#output");
 
-var serverURL = "https://api.funtranslations.com/translate/shakespeare.json"
+var serverURL = "https://api.funtranslations.com/translate/gungan.json"
 
 
 function getTranslationURL(input) {
